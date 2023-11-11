2670b927441b027821be599efac1f7825e36c571
Bilalb <33909526+bilalbg@users.noreply.github.com>
2023-11-10 22:08:51 -0500

    Update Data.js

--- src/Components/infosec/Data.js
+++ src/Components/infosec/Data.js
@@ -9,7 +9,7 @@ export const homeObjOne = {
   headline:'Who is Bilal?',
   description:"I am a Software Development Engineer, I graduated from York University and worked at Amazon from 2022-2023."+
   " I have always been fascinated by technology since I was a child and ever since I have "+
-  "enjoyed learning as much about it as I could.\n \n"+
+  "enjoyed learning as much about it as I could.\\n \\n"+
   "On a more personal note, in my free time I like driving and working on motorcycles(that\'s me on the right!) and cars, reading (often fiction or philosophical books), playing video games( I\'m a huge RPG fan),"+
     " or playing my guitar and piano. I also frequently exercise by going to the gym and occasionally biking.",
   imgStart:false,
