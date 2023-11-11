2a25ceb7c2c5026ae04a8cff9f5d9ef34743089c
Bilalb <33909526+bilalbg@users.noreply.github.com>
2023-11-10 23:50:02 -0500

    Update index.js

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaBars } from 'react-icons/fa';
+// import { FaBars } from 'react-icons/fa';
 // import {IconContext} from 'react-icons/lib';
 import {animateScroll as scroll} from 'react-scroll';
 import Resume from "../../Images/Bilal_Baig_Resume.pdf"
