b48e0efa8776bce2e881788fe560b6f1012d0523
Bilalb <33909526+bilalbg@users.noreply.github.com>
2024-05-24 18:28:48 -0400

    Update mElements.js

--- src/Components/MainSection/mElements.js
+++ src/Components/MainSection/mElements.js
@@ -3,15 +3,11 @@ import styled from 'styled-components'
 
 export const MainContainer = styled.div`
 
-  // background:black;
   height:10vh;
-  // background:black;
   display:flex;
   justify-content:center;
   align-items:center;
-  // padding:0 30px;
   width:intrinsic;
-  // height:100% - 160px;
   position:static;
   z-index:2;
 `
@@ -20,7 +16,6 @@ export const MainContainer = styled.div`
 export const MainContent = styled.div`
   z-index:0;
   max-width:1200px;
-  // position:absolute;
   padding:8px 24px;
   display:flex;
   flex-direction:column;
@@ -40,7 +35,6 @@ export const MainP1 = styled.p`
   color:white;
   font-size:1.5rem;
   text-align:center;
-  // font-weight:bold;
   max-width:600px;
   text-shadow: -1px 1px 20px rgba(0, 0, 0, 1)
 
