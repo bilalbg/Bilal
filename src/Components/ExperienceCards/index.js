59c5876ef6821267e5a0caf1d54bf2eb1b82b83d
Bilalb <33909526+bilalbg@users.noreply.github.com>
2024-07-04 13:03:48 -0400

    update experience

--- src/Components/ExperienceCards/index.js
+++ src/Components/ExperienceCards/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
 import './Cards.css'
-import {amzn} from "../../Images"
+import {amzn, hfla} from "../../Images"
 import {
   Cardcontainer,
   ulist,
@@ -16,19 +16,33 @@ const Cards = () => {
       <Cardcontainer>
         
         <Card className="bg-dark text-white">
+           <Img src={hfla} width="30vw"/>
+            <Card.Body>
+              <Card.Title  >Hack for LA (Volunteer) </Card.Title>
+              <Card.Text >
+                <ulist>
+                <li>Collaborated with a team of researchers, designers and developers to create a Web Traffic Demand Management tool for the LA Department of Transportation using React, Javascript and SQL </li> 
+                </ulist>
+              </Card.Text>
+            </Card.Body>
+        </Card>
+      </Cardcontainer>
+                  
+      <Cardcontainer>
+        
+        <Card className="bg-dark text-white">
            <Img src={amzn} width="30vw"/>
             <Card.Body>
               <Card.Title  >Amazon</Card.Title>
               <Card.Text >
                 <ulist>
-                <li>Developed Event Driven Architecture using <b>Java</b>,  <b>Typescript</b> and  <b>AWS Services</b>
-                ,for controlling global inventory events and ensuring accurate inventory levels</li> 
-                <li>Spearheaded the replacement of an obsolete inventory tracking system with a state machine workflow, enhancing
-                operational accuracy by <b>40% </b> </li> 
-                <li>Planned and executed automation tools, resulting in a <b>20%</b> reduction in on-call hours and the proactive mitigation
-                of business impacts due to discrepancies in data stores for millions of items</li> 
-                <li>Mentored junior engineers through guidance, facilitating their skill development and team success</li> 
-                <li>Identified and addressed failure points to maintain operational excellence during on-call responsibilities</li>
+                <li>Developed Event Driven Architecture using <b>Java</b>,  <b>Typescript</b> and  <b>AWS Services</b>, 
+                  processing thousands of daily global inventory events and ensuring precise inventory levels</li> 
+                <li>Led initiative to automate correcting inaccurate data in inventory stores using AWS Lambda, S3, and DynamoDB,
+                  resulting in the mitigation of business impacts and a 15% reduction in time spent on weekly manual tasks </li> 
+                <li>Designed a second iteration of an inventory tracking system, utilizing AWS Step Functions workflow, enhancing operational accuracy by 40% </li> 
+                <li>Mentored junior engineers through 1:1 biweekly meetings, giving suggestions on code structure, clarity, and design, leading to an overall increase in team productivity and agility</li> 
+                <li>Automated cleaning up 100s of weekly log files with CronJobs, mitigating service certificate errors due to storage capacity during yearly code freezes</li>
                 </ulist>
               </Card.Text>
             </Card.Body>
