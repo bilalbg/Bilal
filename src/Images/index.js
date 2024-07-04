9e2cd891a058f4886fc09ca5be92bc5763b6de23
Bilalb <33909526+bilalbg@users.noreply.github.com>
2024-07-04 13:03:21 -0400

    add hfla logo

--- src/Images/index.js
+++ src/Images/index.js
@@ -1,9 +1,11 @@
 import background from  './background.jpg' ;
 import bio from './svg-bio.svg' ;
 import amzn from './AMZN.svg';
+import hfla from './HFLA.png';
 
 export  {
   background,
   bio,
   amzn,
+  hfla,
 }
